3d66cea6bf37136328a8315dccfc94007fc449b8
Patrick Germann <30571074+patrickgermann@users.noreply.github.com>
2020-07-07 06:40:24 +0200

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Patrick Germann on 07.07.20.
+//  Copyright © 2020 Patrick Germann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
